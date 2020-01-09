dea0f1e3a1f8e2d413426077282f64d8191ef45e
edutco <edut97@gmail.com>
2020-01-09 14:46:34 +0200

    one robot is ongoing

--- src/gameClient/GameManager.java
+++ src/gameClient/GameManager.java
@@ -1,5 +1,6 @@
 package gameClient;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -17,53 +18,87 @@ public class GameManager implements Runnable {
 		Master.start();
 	} 
 
-	private static double nearestFruit (int src) {
-		int dest=nextDest(src);
-		if(dest !=-1)
-			return MyGameGUI.algo.myGraph.getEdge(src,dest).getWeight();
-		else {
-			double min=Double.POSITIVE_INFINITY;
-			Collection <edge_data> edges=MyGameGUI.algo.myGraph.getE(src);
-			for (Iterator<edge_data> iterator = edges.iterator(); iterator.hasNext();) {
-				edge_data edge = (edge_data) iterator.next();
-				double curr= nearestFruit(edge.getDest())+edge.getWeight();
-				if(curr<min)
-					min=curr;				
-			}
-			return min;
+	private static List<Integer>  getFruitSrc(){
+		Collection<String> fruits=MyGameGUI.game.getFruits();
+		ArrayList<Integer> list= new ArrayList<Integer>();
+		for (String f : fruits) {
+			JSONObject line;
+			try {
+				line = new JSONObject(f);
+				JSONObject ttt = line.getJSONObject("Fruit");
+				double val = ttt.getDouble("value");
+				int type = ttt.getInt("type");
+				String loc = (String) ttt.get("pos");
+				Point3D floc= new Point3D(loc);
+				list.add(findFruitSrc(floc,val, type));
+			} 
+			catch (JSONException e) {e.printStackTrace();}
 		}
+		return list;
 	}
+
+
+	private static int findFruitSrc(Point3D floc,double val, int type) {
+		int ans=-1;
+		for (int i = 0; i < MyGameGUI.algo.myGraph.nodeSize(); i++) {
+			Collection<edge_data> e =MyGameGUI.algo.myGraph.getE(i);
+			for (Iterator<edge_data> iterator = e.iterator(); iterator.hasNext();) {
+				edge_data currE = (edge_data) iterator.next();
+				if(isOnEdge(floc, val, type, currE)!=0)
+					return i;
+			}
+		}
+		if (ans==-1)
+			System.out.println("loc="+floc+" type="+type+" is not on an edge:(");
+		return ans;
+	}
+
+	//		private static double nearestFruit (int src) {
+	//			int dest=nextDest(src);
+	//			if(dest !=-1) {
+	//				System.out.println("dest is"+ dest);
+	//				return MyGameGUI.algo.myGraph.getEdge(src,dest).getWeight();
+	//			}
+	//			else {
+	//				double min=Double.POSITIVE_INFINITY;
+	//				Collection <edge_data> edges=MyGameGUI.algo.myGraph.getE(src);
+	//				for (Iterator<edge_data> iterator = edges.iterator(); iterator.hasNext();) {
+	//					edge_data edge = (edge_data) iterator.next();
+	//					double curr= nearestFruit(edge.getDest())+edge.getWeight();
+	//					if(curr<min)
+	//						min=curr;				
+	//				}
+	//				return min;
+	//			}
+	//			//return 10000;
+	//		}
 	private static int nextDest(int src) {
+		int ans=-1;
+		double maxVal=0;
 		Collection<edge_data> ee = MyGameGUI.algo.myGraph.getE(src);
 		for (Iterator<edge_data> iterator = ee.iterator(); iterator.hasNext();) {
 			edge_data edge = (edge_data) iterator.next();
+			double edgeVal=0;
 			Collection<String> fruits=MyGameGUI.game.getFruits();
 			for (String f : fruits) {
 				JSONObject line;
 				try {
 					line = new JSONObject(f);
 					JSONObject ttt = line.getJSONObject("Fruit");
-//					double val = ttt.getDouble("value");
+					double val = ttt.getDouble("value");
 					int type = ttt.getInt("type");
 					String loc = (String) ttt.get("pos");
-					String[] s= loc.split(",", 3);
-					double x=Double.parseDouble(s[0]);
-					double y=Double.parseDouble(s[1]);
-					if((edge.getSrc()>edge.getDest() && type==-1)||(edge.getSrc()<edge.getDest() && type==1)) {
-						double sepY;
-						Point3D sl= MyGameGUI.algo.myGraph.getNode(src).getLocation();
-						Point3D dl= MyGameGUI.algo.myGraph.getNode(edge.getDest()).getLocation();
-						sepY= ((sl.y()-dl.y())/(sl.x()-dl.y())  *(x-sl.x())+sl.y());
-						if(Math.abs(sepY-y)<0.000001)
-							return edge.getDest();
-					}
-
+					Point3D floc= new Point3D(loc);
+					edgeVal=+isOnEdge(floc, val, type, edge);
 				}
-				catch (Exception e) {
-				}
+				catch (Exception e) {System.out.println(e.toString());}
+			}
+			if(edgeVal>maxVal) {
+				maxVal=edgeVal;
+				ans=edge.getDest();
 			}
 		}
-		return -1;
+		return ans;
 	}
 	private static int randomNextNode(int src) {
 		int ans = -1;
@@ -92,15 +127,31 @@ public class GameManager implements Runnable {
 							int rid = ttt.getInt("id");
 							int src = ttt.getInt("src");
 							int dest = ttt.getInt("dest");
-
 							if(dest==-1) {	
 								if(nextDest(src)!=-1) {
 									System.out.println("not randomm!!!!");
 									MyGameGUI.game.chooseNextEdge(rid, nextDest(src));
 								}
-								else {
-									dest = randomNextNode(src);
-									MyGameGUI.game.chooseNextEdge(rid, dest);
+								else
+								{
+									System.out.println("fruits are in: "+getFruitSrc());
+									int favNode= whereToGo(src, getFruitSrc());
+									System.out.println("heading to "+favNode);
+//									int firstStep= MyGameGUI.algo.shortestPath(src, favNode).get(1).getKey();
+//									if(src==firstStep) {
+//										System.out.println("ERROR!!!!!!");
+									//	dest = randomNextNode(src);
+									//	MyGameGUI.game.chooseNextEdge(rid, dest);
+//									}
+//									else {
+//										if(MyGameGUI.algo.myGraph.getEdge(src, firstStep)==null) {
+//											System.out.println("ERROR222222");
+//											dest = randomNextNode(src);
+//											MyGameGUI.game.chooseNextEdge(rid, dest);
+//										}
+//										else
+											MyGameGUI.game.chooseNextEdge(rid, favNode);
+//									}
 								}
 							}
 
@@ -109,6 +160,35 @@ public class GameManager implements Runnable {
 				}
 			}
 		}
+	}
+	private int whereToGo(int src, List<Integer> fruitSrc) {
+		double minDist=Double.MAX_VALUE;
+		int togo=-1;
+		for (int i = 0; i < fruitSrc.size(); i++) {
+			double currDest= MyGameGUI.algo.shortestPathDist(src, fruitSrc.get(i));
+			if(currDest<minDist) {
+				minDist=currDest;
+				togo=fruitSrc.get(i);
+			}
+		}
+		//return togo;
+				int firstStep;
+				System.out.println("size of moves ="+MyGameGUI.algo.shortestPath(src, togo).size());
+				if(MyGameGUI.algo.shortestPath(src, togo).size()>1)
+					firstStep=MyGameGUI.algo.shortestPath(src, togo).get(1).getKey(); //get the second node on the list going from src to the edge has a fruit
+				else
+					firstStep=MyGameGUI.algo.shortestPath(src, togo).get(0).getKey();
+				return firstStep;
+	}
+
+	private static double isOnEdge(Point3D fruit, double value, int type,  edge_data e ) {
+		Point3D src=MyGameGUI.algo.myGraph.getNode(e.getSrc()).getLocation();
+		Point3D dest=MyGameGUI.algo.myGraph.getNode(e.getDest()).getLocation();
+		if ( Math.abs(fruit.distance2D(src) + fruit.distance2D(dest)-src.distance2D(dest))<0.001) { //fruit is on the edge
+			if((e.getSrc()>e.getDest() && type==-1)||(e.getSrc()<e.getDest() && type==1)) //type of fruit matches edge
+				return value;
+		}
+		return 0;
 	}
 }
 
