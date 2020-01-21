e454cb9128c0b817a5d3245ebf5b7d5fe5ffea8d
idoshapira051 <idshapira051@gmail.com>
2020-01-21 21:34:43 +0200

    show only boaz levels

--- src/gameClient/DBquery.java
+++ src/gameClient/DBquery.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -29,14 +30,63 @@ public class DBquery {
 	private static enum col{ //all the columns in the Logs table
 		levelID, moves, time, score, UserID, logID,
 	}
+	private static HashMap<Integer, ArrayList<Integer>> toSuccsessLevel = new HashMap<Integer,ArrayList<Integer>>();
+	private static void initiateSuccsessParam() {
+		//level 0:
+		toSuccsessLevel.put(0,new ArrayList<Integer>());
+		toSuccsessLevel.get(0).add(145);
+		toSuccsessLevel.get(0).add(290);
+		//level 1:
+		toSuccsessLevel.put(1,new ArrayList<Integer>());
+		toSuccsessLevel.get(1).add(450);
+		toSuccsessLevel.get(1).add(580);
+		//level 3:
+		toSuccsessLevel.put(3,new ArrayList<Integer>());
+		toSuccsessLevel.get(3).add(720);
+		toSuccsessLevel.get(3).add(580);
+		//level 5:
+		toSuccsessLevel.put(5,new ArrayList<Integer>());
+		toSuccsessLevel.get(5).add(570);
+		toSuccsessLevel.get(5).add(500);
+		//level 9:
+		toSuccsessLevel.put(9,new ArrayList<Integer>());
+		toSuccsessLevel.get(9).add(510);
+		toSuccsessLevel.get(9).add(580);
+		//level 11:
+		toSuccsessLevel.put(11,new ArrayList<Integer>());
+		toSuccsessLevel.get(11).add(1050);
+		toSuccsessLevel.get(11).add(580);
+		//level 13:
+		toSuccsessLevel.put(13,new ArrayList<Integer>());
+		toSuccsessLevel.get(13).add(310);
+		toSuccsessLevel.get(13).add(580);
+		//level 16:
+		toSuccsessLevel.put(16,new ArrayList<Integer>());
+		toSuccsessLevel.get(16).add(235);
+		toSuccsessLevel.get(16).add(290);
+		//level 19:
+		toSuccsessLevel.put(19,new ArrayList<Integer>());
+		toSuccsessLevel.get(19).add(250);
+		toSuccsessLevel.get(19).add(580);
+		//level 20:
+		toSuccsessLevel.put(20,new ArrayList<Integer>());
+		toSuccsessLevel.get(20).add(200);
+		toSuccsessLevel.get(20).add(290);
+		//level 23:
+		toSuccsessLevel.put(23,new ArrayList<Integer>());
+		toSuccsessLevel.get(23).add(1000);
+		toSuccsessLevel.get(23).add(1140);
+	}
 
 	public DBquery(String idForInfo) {
+//		initiateSuccsessParam();
 		this.id = idForInfo;
 	}
+	
 	/**
 	 * in the start this method is activated in order to get a quick access to the table
 	 * @param table the name of the table
-	 * @return a copy of the table from the server
+	 * @return a copy of the table from the server only for The students who passed the level
 	 */
 	private static ArrayList<ArrayList<String>> getTable(String table) {
 		ArrayList<ArrayList<String>> ans = new ArrayList<ArrayList<String>>();
@@ -68,6 +118,26 @@ public class DBquery {
 		}
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		}
+		//return keepOnlyStudentsPassed(ans);
+		return ans;
+	}
+	/**
+	 * this method get the all table and return the students only if they passed the level
+	 * @param ans
+	 * @return
+	 */
+	private static ArrayList<ArrayList<String>> keepOnlyStudentsPassed(ArrayList<ArrayList<String>> table) {
+		ArrayList<ArrayList<String>> ans = new ArrayList<ArrayList<String>>();
+		System.out.println(toSuccsessLevel);
+		for (Iterator<ArrayList<String>> iterator = ans.iterator(); iterator.hasNext();) {
+			ArrayList<String> row = (ArrayList<String>) iterator.next();
+			int level = Integer.parseInt(row.get(col.levelID.ordinal()));
+			int points = Integer.parseInt(row.get(col.score.ordinal()));
+			int moves = Integer.parseInt(row.get(col.moves.ordinal()));
+			if(toSuccsessLevel.get(level).get(0) <= points && toSuccsessLevel.get(level).get(1) >= moves) {
+				ans.add(row);
+			}
 		}
 		return ans;
 	}
@@ -155,8 +225,14 @@ public class DBquery {
 		}
 		int level = 0;
 		ArrayList<Double> allBestscoreByIdLevel = this.getBestScoreById(this.id);
+		while(allBestscoreByIdLevel.remove((double)-1));
 		ans.append("\t3) Best out come in each level:\n");
+		System.out.println(allBestscoreByIdLevel);
 		for (Iterator<Double> iterator = allBestscoreByIdLevel.iterator(); iterator.hasNext();) {
+			if(level == 2 || level == 4 || (level >= 6 && level <= 8) || level == 10 || level == 12 || level == 14 || level == 15 || level == 17 || level == 18 || level == 21 || level == 22) {
+				level++;
+				continue;
+			}
 			Double score = (Double) iterator.next();
 			if(score != -1) {
 				ans.append("\t\tLevel "+level +") best score: "+ score + '\n');
@@ -169,7 +245,9 @@ public class DBquery {
 		return ans.substring(0);
 	}
 	/**
-	 * for each level this method return his position in a relation to the rest of the class
+	 * for each level this method return his position in a relation to the rest of the class.
+	 * The position is determined by the score that exists and does not refer to the number of moves
+	 * assuming that each student in the table has passed the stage played.
 	 */
 	public String getPosition() {
 		StringBuilder ans = new StringBuilder();
@@ -182,12 +260,15 @@ public class DBquery {
 			allusersBestScores.add(this.getBestScoreById(id));
 		}
 		for (int i = 0; i <= NumberOfLevels; i++) {
+			if(i == 2 || i == 4 || (i >= 6 && i <= 8) || i == 10 || i == 12 || i == 14 || i == 15 || i == 17 || i == 18 || i == 21 || i == 22) {
+				continue;
+			}
 			int countGreater = 1;
 			if(myBestScoreByLevel.get(i) != -1.0) {
 				for (Iterator<ArrayList<Double>> iterator = allusersBestScores.iterator(); iterator.hasNext();) {
 					ArrayList<Double> allLevelsCurrentPlayer = (ArrayList<Double>) iterator.next();
 					Double currentLevel = allLevelsCurrentPlayer.get(i);
-					if(currentLevel >= myBestScoreByLevel.get(i)) {
+					if(currentLevel > myBestScoreByLevel.get(i)) {
 						countGreater++;
 					}
 				}
--- src/gameClient/SimpleGameClient.java
+++ src/gameClient/SimpleGameClient.java
@@ -1,5 +1,23 @@
 package gameClient;
 
+/**
+* This class represents a simple example for using the GameServer API:
+* the main file performs the following tasks:
+* 0. login as a user ("999") for testing - do use your ID.
+* 1. Creates a game_service [0,23] (user "999" has stage 10, can play in scenarios [0,10] not above
+* 2. Constructs the graph from JSON String
+* 3. Gets the scenario JSON String 
+* 5. Add a set of robots  // note: in general a list of robots should be added
+* 6. Starts game 
+* 7. Main loop (vary simple thread)
+* 8. move the robot along the current edge 
+* 9. direct to the next edge (if on a node) 
+* 10. prints the game results (after "game over"), and write a KML: 
+*  
+* @author boaz.benmoshe
+*
+*/
+
 public class SimpleGameClient {
 	public static void main(String[] a) {
 		MyGameGUI gui = MyGameGUI.getGui(); //starts the gui thread 
