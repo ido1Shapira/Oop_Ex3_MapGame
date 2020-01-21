c0a7b119b097cf75f7fc1feafac400131d129534
edutco <edut97@gmail.com>
2020-01-21 16:46:53 +0200

    new algo

--- src/myUtils/myRobot.java
+++ src/myUtils/myRobot.java
@@ -1,18 +1,39 @@
 package myUtils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import Server.robot;
 import oop_utils.OOP_Point3D;
 import utils.Point3D;
 
 public class myRobot {
-	 private int id;
-	 private double value;
-	 private int src;
-	 private int dest;
-	 private Point3D pos;
-	 private double speed;
-	 private boolean hasDest;
-	 
+	private int id;
+	private double value;
+	private int src;
+	private int dest;
+	private Point3D pos;
+	private double speed;
+	private boolean hasDest;
+
+	public myRobot(String jRobot) {
+		JSONObject line;
+		try {
+			line = new JSONObject(jRobot);
+			JSONObject robi= line.getJSONObject("Robot");
+			id= robi.getInt("id");
+			value= robi.getDouble("value");
+			speed= robi.getDouble("speed");
+			src= robi.getInt("src");
+			dest= robi.getInt("dest");
+			pos= new Point3D(robi.getString("pos"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 	public int getID() {
 		return id;
 	}
@@ -25,7 +46,7 @@ public class myRobot {
 		return value;
 	}
 
-	public int getNextNode() {
+	public int getDest() {
 		return dest;
 	}
 
@@ -51,7 +72,7 @@ public class myRobot {
 	public void setSpeed(double arg0) {
 		speed= arg0;
 	}
-	
+
 	public void setLocation(Point3D pos1) {
 		pos= pos1;
 	}
